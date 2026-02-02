36a274598ca97c56af4da2c92deb0b60b1f1deaa
BOUCHAMI-Enzo-2326025aa <enzo.bouchami@etu.univ-amu.fr>
2026-02-02 10:55:02 +0100

    fix bug upload video

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -6,6 +6,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
